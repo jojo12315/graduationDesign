04682c3522b81a2d8c40eb8a99ebc4845a1601ab
jojo12315 <55019179+jojo12315@users.noreply.github.com>
2022-03-25 12:15:35 +0800

    init repo

---
+++ src/main/java/com/example/campusservicesystem/CampusServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.campusservicesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusServiceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusServiceSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/campusservicesystem/CampusServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campusservicesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusServiceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
